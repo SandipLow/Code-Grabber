e8c550a79c9379954abc876a2c263854bdc12a5a
Sandip Low <sandipbkt2002@gmail.com>
2022-06-05 09:36:05 +0530

    dynamic port fix for deployment

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const express = require('express')
 connectToMongo()
 
 const app = express();
-const port = 5000;
+const port = process.env.PORT || 5000;
 
 app.use((req, res, next)=>{
     res.setHeader('Access-Control-Allow-Origin', '*');
